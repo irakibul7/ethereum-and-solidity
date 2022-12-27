fc54989487457469b4bc5bb195d765647454bada
irakibul7 <irakibul568@gmail.com>
2022-12-27 18:18:13 +0600

    generate bitcoin cash address

--- src/bitcoin_address.js
+++ src/bitcoin_address.js
@@ -2,6 +2,7 @@ const bip39 = require('bip39');
 const bitcoin = require('bitcoinjs-lib');
 const { BIP32Factory } = require('bip32');
 const ecc = require('tiny-secp256k1');
+const bchaddr = require('bchaddrjs');
 require('dotenv').config();
 // You must wrap a tiny-secp256k1 compatible implementation
 const bip32 = BIP32Factory(ecc);
@@ -22,4 +23,12 @@ const bip84_address = bitcoin.payments.p2wpkh({
   pubkey: keypair2.publicKey,
 }).address;
 
-console.log(`Address using bip84: ${bip84_address}`);
+console.log(`Bitcoin Address using bip84: ${bip84_address}`);
+
+const keypair3 = bip32.fromSeed(seed).derivePath("m/44'/145'/0'/0/0");
+const bip145_address = bitcoin.payments.p2pkh({
+  pubkey: keypair3.publicKey,
+}).address;
+const toCashAddress = bchaddr.toCashAddress;
+const cashAddress = toCashAddress(bip145_address);
+console.log(`Bitcoin Cash Address: ${cashAddress}`);
